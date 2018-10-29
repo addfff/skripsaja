3ec8a6151da570417988903f43f0fa5ca7462f79
sir <nadahahs@gmail.com>
2018-10-29 19:35:31 +0800

    fix line kat jail.conf

--- try.sh
+++ try.sh
@@ -16,8 +16,8 @@ j=/etc/jail.conf
 echo $namakau '{' >> $j
 echo '    host.hostname = '$namakau'.nFWG112;                # Hostname' >> $j
 echo '    ip4.addr = '$ipdia';                   # IP address of the jail' >> $j
-echo '    path ="/jails/participant-'$namakau'";                         # Path
-echo '    mount.devfs;                               # Mount devfs inside the ja
+echo '    path ="/jails/participant-'$namakau'";                         # Path to the jail' >> $j
+echo '    mount.devfs;                               # Mount devfs inside the jail' >> $j
 echo '    exec.start = "/bin/sh /etc/rc";            # Start command' >> $j
 echo '    exec.stop = "/bin/sh /etc/rc.shutdown";    # Stop command' >> $j
 echo '    allow.raw_sockets=1;' >> $j
