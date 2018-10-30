32d6217f5fd2b9e50f6457bb3787e8958fa766f5
sir <nadahahs@gmail.com>
2018-10-30 16:10:06 +0800

    buang comment

--- try.sh
+++ try.sh
@@ -14,12 +14,12 @@ cpdup /jails/participant01 $D
 mount -t devfs devfs $D/dev
 j=/etc/jail.conf
 echo $namakau '{' >> $j
-echo '    host.hostname = '$namakau'.nFWG112;                # Hostname' >> $j
-echo '    ip4.addr = '$ipdia';                   # IP address of the jail' >> $j
-echo '    path ="/jails/participant-'$namakau'";                         # Path to the jail' >> $j
-echo '    mount.devfs;                               # Mount devfs inside the jail' >> $j
-echo '    exec.start = "/bin/sh /etc/rc";            # Start command' >> $j
-echo '    exec.stop = "/bin/sh /etc/rc.shutdown";    # Stop command' >> $j
+echo '    host.hostname = '$namakau'.nFWG112; >> $j
+echo '    ip4.addr = '$ipdia'; >> $j
+echo '    path ="/jails/participant-'$namakau'"; >> $j
+echo '    mount.devfs; >> $j
+echo '    exec.start = "/bin/sh /etc/rc"; >> $j
+echo '    exec.stop = "/bin/sh /etc/rc.shutdown"; >> $j
 echo '    allow.raw_sockets=1;' >> $j
 echo '}' >> $j
 service jail restart
