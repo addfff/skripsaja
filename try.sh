4e0fa42812c8518ec137391c7dca5fef44604038
sir <nadahahs@gmail.com>
2018-11-01 03:49:58 +0800

    everything in try.sh

--- try.sh
+++ try.sh
@@ -4,6 +4,10 @@ echo "nama apa"
 read namakau
 echo "ip dia"
 read ipdia
+echo "alias"
+read ipalias
+echo 'ifconfig_em0_alias'$ipalias'="inet '$ipdia' netmask 255.255.255.255"' >> /etc/rc.conf
+/etc/rc.d/netif restart
 D=/jails/participant-$namakau
 echo $D
 pwd
@@ -14,13 +18,16 @@ cpdup /jails/participant01 $D
 mount -t devfs devfs $D/dev
 j=/etc/jail.conf
 echo $namakau '{' >> $j
-echo '    host.hostname = '$namakau'.nFWG112; >> $j
-echo '    ip4.addr = '$ipdia'; >> $j
-echo '    path ="/jails/participant-'$namakau'"; >> $j
-echo '    mount.devfs; >> $j
-echo '    exec.start = "/bin/sh /etc/rc"; >> $j
-echo '    exec.stop = "/bin/sh /etc/rc.shutdown"; >> $j
+echo '    host.hostname = '$namakau'.nFWG112;' >> $j
+echo '    ip4.addr = '$ipdia';' >> $j
+echo '    path ="/jails/participant-'$namakau'";' >> $j
+echo '    mount.devfs;' >> $j
+echo '    exec.start = "/bin/sh /etc/rc";' >> $j
+echo '    exec.stop = "/bin/sh /etc/rc.shutdown";' >> $j
 echo '    allow.raw_sockets=1;' >> $j
 echo '}' >> $j
+echo 'PermitRootLogin yes' >> $D/etc/ssh/sshd_config
+echo 'sshd_enable="YES"' > $D/etc/rc.conf
+#chflags schg $D/etc/master.passwd
+#chflags schg $D/etc/passwd
 service jail restart
-
